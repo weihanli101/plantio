651c18f1b5495ae528cff98d660073940e84ac71
weihan <weihanli101@gmail.com>
2019-04-19 01:40:01 -0400

    fixed index local build route + removed root index.html + npm audit fixes

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,7 @@ if(process.env.NODE_ENV === 'production') {
 
 // build mode
 router.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname+'/client/public/index.html'));
+	res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
-
-
-
 
 module.exports = router;
