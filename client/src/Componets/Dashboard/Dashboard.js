95d12bb5f228eb6bb9f93e6573fbecee0d514840
weihan <weihanli101@gmail.com>
2019-08-19 19:11:38 -0400

    routing tests

--- client/src/Componets/Dashboard/Dashboard.js
+++ client/src/Componets/Dashboard/Dashboard.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import SearchBarView from '../SearchBar/SearchBarView';
 import PlantCard from '../PlantCard/PlantCard';
 
-import './Dashboard.css';
-
 class Dashboard extends Component {
 
     render(){
