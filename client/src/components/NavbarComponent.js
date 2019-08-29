a4968e19372aa9b50884497f537c77d26c6913e9
Weihan Li <weihan@roadmunk.com>
2019-08-29 18:50:42 -0400

    adjusted navbar

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -3,37 +3,43 @@ import { useAuth0 } from '../react-auth0-wrapper';
 import { Navbar, Button, Alignment } from "@blueprintjs/core";
 
 const NavbarComponent = () => {
-	const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-	return (
-		<Navbar>
-		<Navbar.Group align={Alignment.LEFT}>
-			<Navbar.Heading>Plantio</Navbar.Heading>
-			<Navbar.Divider />
-		</Navbar.Group>
-		{
-			isAuthenticated && (
-				<Navbar.Group align={Alignment.RIGHT}>
-					<Button className='bp3-minimal bp3-icon-notifications'/>
-					<Button className='bp3-minimal bp3-icon-cog'/>
-					<Button className='bp3-minimal bp3-icon-user'/>
-					<Button className="bp3-minimal" rightIcon="log-out" text="Logout" onClick={
-						() => logout()
-					}/>
-				</Navbar.Group>
-			)
-		}
-		{
-			!isAuthenticated && (
-				<Navbar.Group align={Alignment.RIGHT}>
-					<Button className="bp3-minimal" text="Dashboard" rightIcon="arrow-right" onClick={() =>
-						loginWithRedirect({})
-					}/>
-				</Navbar.Group>
-			)
-		}
-		</Navbar>
-	)
+    return (
+        <Navbar>
+            <Navbar.Group align={Alignment.LEFT}>
+                <Navbar.Heading>Plantio</Navbar.Heading>
+                <Navbar.Divider />
+            </Navbar.Group>
+            {
+                isAuthenticated && (
+                    <Navbar.Group align={Alignment.RIGHT}>
+                        <Button className='bp3-minimal bp3-icon-notifications' />
+                        <Button className='bp3-minimal bp3-icon-cog' />
+                        <Button className='bp3-minimal bp3-icon-user' />
+                        <Button className="bp3-minimal" rightIcon="log-out" text="Logout" onClick={
+                            () => logout()
+                        } />
+                    </Navbar.Group>
+                )
+            }
+            {
+                !isAuthenticated && (
+                    <Navbar.Group align={Alignment.RIGHT}>
+                        <Button className="bp3-minimal" text="Dashboard" rightIcon="arrow-right" onClick={() =>
+                            loginWithRedirect({})
+                        } />
+                    </Navbar.Group>
+                )
+            }
+        </Navbar>
+    )
+
+    private renderCurrentBreadcrumb = ({ text, ...restProps }: IBreadcrumbProps) => {
+        // customize rendering of last breadcrumb
+        return <Breadcrumb {...restProps}>{text} <Icon icon="star" /></Breadcrumb>;
+    };
+
 }
 
 export default NavbarComponent;
